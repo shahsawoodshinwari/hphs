8e83a7b240fc07e17ed0bbb5d5bd53170009aee8
shinwaris <zoology.fb@gmail.com>
2022-08-27 01:00:49 +0500

    changed navbar to bootstrap navbar

--- javascript/school.js
+++
@@ -1,8 +0,0 @@
-var click = 0;
-const pictures = ["1.jpg", "2.jpg", "5.jpg"];
-function shuffle() {
-	click >= pictures.length - 1 ? click = 0 : click++;
-	var imageName = "url(\'../images/" + pictures[click] + "\')";
-	console.log(imageName);
-	document.getElementById('main').style.backgroundImage = imageName;
-}
