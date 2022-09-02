3bfe069b0fe1c58d31118036afcb71fd5f0e60cc
shinwaris <zoology.fb@gmail.com>
2022-09-03 00:28:56 +0500

    add new css

---
+++ javascript/index.js
@@ -0,0 +1,13 @@
+function setCoverHeight(selector) {
+    const screen_height = window.innerHeight;
+    const nav = document.querySelector('nav');
+    const navbar_height = nav.offsetHeight;
+    const other_height = document.querySelector('div#cover div.col-12.d-block.d-md-none').offsetHeight;
+    const cover_height = screen_height - (navbar_height + other_height);
+    const cover = document.querySelector(selector);
+    cover.setAttribute('style', `height:${cover_height}px !important; width:100% !important;`);
+    return cover;
+}
+
+document.addEventListener('DOMContentLoaded', setCoverHeight('#cover img'));
+window.addEventListener('resize', setCoverHeight('#cover img'));
