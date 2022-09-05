1d6a1faea7c23ace18b73c8c7f837a5a2fdf8251
shinwaris <zoology.fb@gmail.com>
2022-09-05 10:49:21 +0500

    ...

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,15 @@
-function setIframeHeight(iframe) {
-    iframe.height = iframe.contentWindow.document.body.scrollHeight;
-}
+    function setCoverHeight(selector) {
+        const screen_height = window.innerHeight;
+        const nav = document.querySelector('nav');
+        const navbar_height = nav.offsetHeight;
+        const other_height = document.querySelector('div#cover div.col-12.d-block.d-md-none').offsetHeight;
+        const cover_height = screen_height - (navbar_height + other_height);
+        const cover = document.querySelector(selector);
+        cover.setAttribute('style', `height:${cover_height}px !important; width:100% !important;`);
+        return cover;
+    }
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('iframe').forEach(iframe => {
-        setIframeHeight(iframe);
-    })
-})
+    document.addEventListener('DOMContentLoaded', () => {
+        setCoverHeight('#cover img');
+    });
+    window.onresize = setCoverHeight('#cover img');
