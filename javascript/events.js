dff58566834b9505d93279ef7b086e166bf1ce6f
shinwaris <zoology.fb@gmail.com>
2022-09-06 14:35:07 +0500

    removed console.log

--- javascript/events.js
+++ javascript/events.js
@@ -15,7 +15,6 @@ function create_card(title, images) {
     const row = document.createElement('div');
     row.classList.add("row", "row-cols-1", "row-cols-sm-2", "row-cols-md-3", "row-cols-lg-4", "g-1", "align-items-center");
 
-    console.log(images.length)
     images.forEach(image => {
         const innerCol = document.createElement('div');
         innerCol.classList.add('col')
