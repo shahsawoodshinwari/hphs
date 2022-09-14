79e4e1c122d3a9801182b5b780507b0ec63e3177
shinwaris <zoology.fb@gmail.com>
2022-09-14 21:33:40 +0500

    made result cards dynamic

--- javascript/results.js
+++ javascript/results.js
@@ -1,13 +1,37 @@
-function create_result_card(obj) {
-    grades = "";
-    obj.classes.forEach(grade => {
-        //
-    });
-    const card = `<div class="col-12"><div class="card" style="border-color:var(--bs-orange);"><div class="card-header" style="border-color:var(--bs-orange);">${obj.year}</div><div class="card-body"><div class="row g-2"><div class="col-12 col-md-4"><div class="card" style="border-color:var(--bs-orange);"><div class="card-header text-center" style="border-color:var(--bs-orange);">Prep</div><div class="row g-1"><div class="col-md-4"><img src="images/default.jpg" class="img-fluid" alt=""><div class="card-text text-center">1st</div></div><div class="col-md-4"><img src="images/default.jpg" class="img-fluid" alt=""><div class="card-text text-center">2nd</div></div><div class="col-md-4"><img src="images/default.jpg" class="img-fluid" alt=""><div class="card-text text-center">3rd</div></div></div></div></div></div></div></div></div>`;
+function createResultCard(obj) {
+
+    // create a class card that contains position holders
+    function createClassResultCard(grade, positionHolders) {
+
+        // create a position holder card
+        function createPositionHolderCard(position_holder) {
+            return `<div class="col-4 col-md-4"><div class="card"><img src="${position_holder.image}" class="img-fluid card-img-top" alt=""><div class="card-body p-1"><div class="card-text text-center text-truncate">${position_holder.name}</div></div><div class="card-footer"><div class="card-text text-center">${position_holder.position}</div></div></div></div>`;
+        }
+
+        var positionHoldersCollection = '';
+        positionHolders.forEach(position_holder => {
+            positionHoldersCollection += createPositionHolderCard(position_holder);
+        });
+        return `<div class="col-12 col-md-4"><div class="card" style="border-color:var(--bs-orange);" bgcolor="red"><div class="card-header text-center" style="border-color:var(--bs-orange);">${grade}</div><div class="card-body p-1"><div class="row g-1">${positionHoldersCollection}</div></div></div></div>`;
+    }
+
+    var grades = "";
+    const keys = Object.keys(obj.classes);
+    for (var i = 0; i < keys.length; i++) {
+        grades += createClassResultCard(keys[i], obj.classes[keys[i]]);
+    }
+    return `<div class="col-12"><div class="card" style="border-color:var(--bs-orange);"><div class="card-header text-center" style="border-color:var(--bs-orange);">${obj.year}</div><div class="card-body p-1"><div class="row g-2">${grades}</div></div></div></div>`;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    results.forEach(result => {
-        create_result_card(result);
-    });
+    var html = '';
+    async function f() {
+        results.forEach(result => {
+            html += createResultCard(result);
+        });
+    }
+    f().then(() => {
+        const target = document.getElementById('result');
+        target.innerHTML = html;
+    })
 });
