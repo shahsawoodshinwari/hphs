a089b298573e030f31e72d23166e893e8cd7592d
shinwaris <zoology.fb@gmail.com>
2022-09-15 00:59:05 +0500

    fixed admission form submission to email

--- javascript/admissions.js
+++ javascript/admissions.js
@@ -9,21 +9,24 @@ function sendAdmissionForm(e) {
     date_of_birth = form.querySelector('#date_of_birth').value;
     current_class = form.querySelector('#current_class').value;
     new_class = form.querySelector('#new_class').value;
-    test_marks = form.querySelector('#test_marks')
-    school = form.querySelector('#school')
-    bodyHTML = `<table border="1" cellpadding="5" cellspacing="0" width="100%"><tbody><tr><td>first name</td><td bgcolor="#f0f0f0">${first_name}</td></tr><tr><td>last name</td><td bgcolor="#f0f0f0">${last_name}</td></tr><tr><td>father name</td><td bgcolor="#f0f0f0">${father_name}</td></tr><tr><td>phone number</td><td bgcolor="#f0f0f0">${phone_number}</td></tr><tr><td>email</td><td bgcolor="#f0f0f0">${email}</td></tr><tr><td>date of birth</td><td bgcolor="#f0f0f0">${date_of_birth}</td></tr><tr><td>current class</td><td bgcolor="#f0f0f0">${current_class}</td></tr><tr><td>new class</td><td bgcolor="#f0f0f0">${new_class}</td></tr><tr><td>test marks</td><td bgcolor="#f0f0f0">${test_marks}</td></tr><tr><td>school</td><td bgcolor="#f0f0f0">${school}</td></tr></tbody></table>`;
+    test_marks = form.querySelector('#test_marks').value;
+    school = form.querySelector('#school').value;
+    bodyHTML = `<ul><li>first name: ${first_name}</li><li>last name: ${last_name}</li><li>father name: ${father_name}</li><li>phone number: ${phone_number}</li><li>email: ${email}</li><li>date of birth: ${date_of_birth}</li><li>current class: ${current_class}</li><li>new class: ${new_class}</li><li>test marks: ${test_marks}</li><li>school: ${school}</li></ul>`;
     key = "047559F4F417EA2B3399709C81F1D8239D6D41BDDECC661AA5020D861730341426BE42E3CF775B04BC317344F3112A5E"
     url = `https://api.elasticemail.com/v2/email/send?apikey=${key}&subject=${first_name} ${last_name} applied onlinee&from=shahsawooddelta446@gmail.com&msgTo=shahsawood@lastwavetechnology.com&bodyHTML=${bodyHTML}`
+    console.log(url)
     fetch(url, {
             method: 'POST'
         })
         .then((response) => response.json())
         .then((data) => {
             if (data.success == true) {
-                document.querySelector('a.nav-link[href="index.html"]').click();
+                console.log('form submit');
+                // document.querySelector('a.nav-link[href="index.html"]').click();
             }
         });
 }
+
 function create_accordion_item(qNo, question, answer) {
     return `<div class="accordion-item">\
     <h2 class="accordion-header" id="flush-heading${ qNo }">\
