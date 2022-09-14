d82ca9b4c1151cdfaff236838b99de3e900ca3a7
shinwaris <zoology.fb@gmail.com>
2022-09-14 22:01:30 +0500

    added spinners before add events

--- javascript/events.js
+++ javascript/events.js
@@ -5,11 +5,17 @@ function create_card(title, images) {
     })
     const card = `<div class="col-12"><div class="card"><div class="card-header">${ title }</div><div class="card-body"><div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-1 align-items-center">${cols}</div></div></div></div>`
 
-    document.querySelector('#events').innerHTML += card;
+    return card;
 } // create card
 document.addEventListener('DOMContentLoaded', () => {
-    events.forEach(obj => {
-        create_card(obj.card_header, obj.images);
-        // console.log(obj.images);
+    e = '<div class="col-12"><div class="text-center text-info fs-2">Events</div></div>';
+    async function f() {
+        events.forEach(obj => {
+            e += create_card(obj.card_header, obj.images);
+        })
+    }
+    f().then(() => {
+        const target = document.querySelector('#events');
+        target.innerHTML = e;
     })
 })
